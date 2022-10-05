5cee33b8598a70956a136bb49af3fecf6613fb60
roror19 <robin.michel.bdx@gmail.com>
2022-10-05 10:51:07 +0200

    mise a jour

---
+++ src/index.js
@@ -0,0 +1,15 @@
+const ul    = document.querySelector(".navEffect");
+const allLi = ul.querySelectorAll("li");
+
+for ( let i = 0; i <allLi.length; i++){
+    let li = allLi[i];
+
+    li.addEventListener("click", function() {
+        
+    })
+}
+
+
+
+    
+ 
