5d92259f5ed8f6326b82a9f75b2bdb3c62c1b4b7
Anita Darecka <anitadarecka@gmail.com>
2022-10-09 09:12:44 +0200

    fix burger menu close animation, add thumbnail for video

--- src/burger.js
+++ src/burger.js
@@ -11,6 +11,14 @@ const faFa = document.querySelector('.fa-bars');
 
 faFa.addEventListener("click", () => {
   const burger = document.getElementById("navBurger");
-  burger.classList.toggle('burgerOut');
-
+  if (burger.style.display === "none") {
+    burger.style.display = "block";
+  } else {
+    burger.classList.add('burgerClose');
+    const changeDisplay = () => {
+      burger.style.display = "none";
+      burger.classList.remove('burgerClose');
+    };
+    setTimeout(changeDisplay, 600);
+  }
 })
